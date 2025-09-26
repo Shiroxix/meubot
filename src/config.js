9fd6166250d2542aaca6212736118fbb72f231a7
Shiroxix <156692327+Shiroxix@users.noreply.github.com>
2025-09-26 19:08:29 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -42,7 +42,7 @@ exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 300;
 exports.SPIDER_API_BASE_URL = "https://api.spiderx.com.br/api";
 
 // Obtenha seu token, criando uma conta em: https://api.spiderx.com.br.
-exports.SPIDER_API_TOKEN = "fZxTRq6PlpGedkoXVc76";
+exports.SPIDER_API_TOKEN = "BVIDZuhNaBU7GtxL5xri";
 
 // Caso queira responder apenas um grupo específico,
 // coloque o ID dele na configuração abaixo.
